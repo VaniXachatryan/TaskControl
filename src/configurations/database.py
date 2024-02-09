741282bc0d13f88db207398f2d6b709dde0932af
Vanik Hachatryan <vanixachatryan@outlook.com>
2024-02-09 17:00:51 +0300

    ADDED: - database and migrations ⚙️ - fast api 🔗 - models/tables 𝌭

---
+++ src/configurations/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from src.configurations.environments import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class BaseModel(DeclarativeBase):
+    pass
+
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+from src.routes.routes import all_routers
+
+app = FastAPI(
+    title="Контроль заданий на выпуск продукции"
+)
+
+for router in all_routers:
+    app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
